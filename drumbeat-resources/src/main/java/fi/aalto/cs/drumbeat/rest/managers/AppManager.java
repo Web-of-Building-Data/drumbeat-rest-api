e3985c132d844ea4bbcb274ddb9c15bad9827007
Nam Vu Hoang <nam.vuhoang@aalto.fi>
2015-12-01 09:41:08 +0200

    Added importing IFC file service

--- drumbeat-resources/src/main/java/fi/aalto/cs/drumbeat/rest/managers/AppManager.java
+++ drumbeat-resources/src/main/java/fi/aalto/cs/drumbeat/rest/managers/AppManager.java
@@ -10,18 +10,21 @@ import fi.hut.cs.drumbeat.rdf.modelfactory.AbstractJenaProvider;
 import fi.hut.cs.drumbeat.rdf.modelfactory.JenaProviderException;
 
 public class AppManager {
-	public static String BASE_URL_TAG = "baseUrl";
 	public static String BASE_URL = "http://localhost:8080/";
+	public static final String BASE_URL_TAG = "baseUrl";
 	
-	public static String WORKING_DIR = "/";
-	public static String WEB_INF_DIR = WORKING_DIR + "/WEB-INF";
-	public static String CONFIG_DIR = WEB_INF_DIR + "/config";	
+	public static final String WORKING_DIR = "/";
+	public static final String WEB_INF_DIR = WORKING_DIR + "/WEB-INF";
+	public static final String CONFIG_DIR = WEB_INF_DIR + "/config";	
 
-	public static String CONFIG_FILE_PATH = CONFIG_DIR + "/config.properties";
-	public static String LOG4J_CONFIG_FILE_PATH = CONFIG_DIR + "/log4j.xml";
+	public static final String CONFIG_FILE_PATH = CONFIG_DIR + "/config.properties";
+	public static final String LOG4J_CONFIG_FILE_PATH = CONFIG_DIR + "/log4j.xml";
+	public static final String IFC2LD_CONFIG_FILE_PATH = CONFIG_DIR + "/ifc2ld.xml";
 
-	public static String JENA_PROVIDER_PREFIX = "jena.provider.";
+	public static final String JENA_PROVIDER_PREFIX = "jena.provider.";
 	
+	public static final String RESOURCES_DIR = WEB_INF_DIR + "/resources";
+	public static final String IFC_SCHEMA_DIR = RESOURCES_DIR + "/ifc";	
 		
 	
 	public static AbstractJenaProvider getJenaProvider(ServletContext servletContext) throws JenaProviderException, IOException {
@@ -30,6 +33,7 @@ public class AppManager {
 		String configFilePath = servletContext.getRealPath(CONFIG_FILE_PATH);
 		FileInputStream in = new FileInputStream(configFilePath);
 		properties.load(in);
+		
 		BASE_URL = properties.getProperty(JENA_PROVIDER_PREFIX + AppManager.BASE_URL_TAG);
 		String providerName = properties.getProperty(JENA_PROVIDER_PREFIX + AbstractJenaProvider.ARGUMENT_PROVIDER_NAME);
 		String providerClassName = properties.getProperty(JENA_PROVIDER_PREFIX + AbstractJenaProvider.ARGUMENT_PROVIDER_CLASS);
