cefe1cf856bcd1fdecf95f5e2bcc889b392e924c
Nam Vu Hoang <nam.vuhoang@aalto.fi>
2015-12-08 13:49:33 +0200

    Changed LBDHO ontology base URI to <http://drumbeat.cs.hut.fi/owl/lbdho.ttl#>

--- drumbeat-resources/src/main/java/fi/aalto/cs/drumbeat/rest/ontology/BuildingDataOntology.java
+++ drumbeat-resources/src/main/java/fi/aalto/cs/drumbeat/rest/ontology/BuildingDataOntology.java
@@ -4,7 +4,7 @@ import fi.aalto.cs.drumbeat.rest.application.DrumbeatApplication;
 
 public class BuildingDataOntology {
 	
-	public static final String BASE_URL = "http://drumbeat.cs.hut.fi/owl/LDBHO#";
+	public static final String BASE_URL = "http://drumbeat.cs.hut.fi/owl/lbdho.ttl#";
 	
 	public static class Collections {
 		public static final String Collection = BASE_URL + "Collection";
