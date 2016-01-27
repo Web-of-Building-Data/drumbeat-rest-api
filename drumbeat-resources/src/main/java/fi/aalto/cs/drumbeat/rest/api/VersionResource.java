070350c2a41d16444884843e4e04462d9cebc482
Nam Vu Hoang <nam.vuhoang@aalto.fi>
2016-01-27 04:34:16 +0200

    * Rename UploadManager to DataSetUploadManager
    * Change version output content to RDF format

--- drumbeat-resources/src/main/java/fi/aalto/cs/drumbeat/rest/api/VersionResource.java
+++ drumbeat-resources/src/main/java/fi/aalto/cs/drumbeat/rest/api/VersionResource.java
@@ -3,23 +3,64 @@ package fi.aalto.cs.drumbeat.rest.api;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.shared.NotFoundException;
 
 import fi.aalto.cs.drumbeat.rest.common.DrumbeatApplication;
+import fi.aalto.cs.drumbeat.rest.common.DrumbeatResponseBuilder;
+import fi.aalto.cs.drumbeat.rest.common.DrumbeatWebException;
 
 @Path("/version")
 public class VersionResource {
 	
 	@GET
 	@Path("/")
-	public String getVersion() throws IOException {
-		Properties properties = new Properties();
-		InputStream in = new FileInputStream(DrumbeatApplication.getInstance().getRealServerPath("classes/version.txt")); 
-		properties.load(in);
-		return properties.toString();
+	public Response getVersion(
+		@Context UriInfo uriInfo,
+		@Context HttpHeaders headers)
+				throws IOException
+	{		
+		DrumbeatApplication.getInstance().notifyRequest(uriInfo);
+		
+		InputStream in = null;		
+		
+		try {
+			in = new FileInputStream(DrumbeatApplication.getInstance().getRealServerPath("classes/version.txt")); 
+
+			Model model = ModelFactory.createDefaultModel();
+			
+			RDFDataMgr.read(model, in, uriInfo.getBaseUri() + "apps/", Lang.TURTLE);
+			
+			model.getNsPrefixMap().put("dcterms", "http://purl.org/dc/terms/");
+			
+			return DrumbeatResponseBuilder.build(
+					Status.OK,
+					model,
+					headers.getAcceptableMediaTypes());			
+		} catch (NotFoundException exception) {
+			throw new DrumbeatWebException(
+					Status.NOT_FOUND,
+					exception.getMessage(),
+					exception);
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+		}
+		
 	}
 	
 
