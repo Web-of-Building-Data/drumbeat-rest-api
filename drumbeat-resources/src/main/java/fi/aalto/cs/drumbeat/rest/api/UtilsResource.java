92d991f553ff4ed7b73e7c4f93e7f0b13bd7e296
Nam Vu Hoang <nam.vuhoang@aalto.fi>
2016-02-19 06:26:27 +0200

    * Upgrade verion number to 0.8.8-SNAPSHOT
    * Implement service utils/ifcguid

---
+++ drumbeat-resources/src/main/java/fi/aalto/cs/drumbeat/rest/api/UtilsResource.java
@@ -0,0 +1,58 @@
+package fi.aalto.cs.drumbeat.rest.api;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
+
+import fi.aalto.cs.drumbeat.common.string.StringUtils;
+import fi.aalto.cs.drumbeat.ifc.common.guid.GuidCompressor;
+import fi.aalto.cs.drumbeat.rest.common.DrumbeatWebException;
+
+@Path("/utils")
+public class UtilsResource {
+	
+	public static final String IFC_GUID_ACTION_COMPRESS = "compress";
+	public static final String IFC_GUID_ACTION_DECOMPRESS = "decompress";
+	
+	@PUT
+	@Path("/ifcguid")	
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String compressIfcGuid(
+			@FormParam("action") String action,
+			@FormParam("guid") String guid)
+	{
+		if (StringUtils.isEmptyOrNull(action)) {
+			throw new DrumbeatWebException(Status.BAD_REQUEST, "Param 'action' is required", null);
+		}
+		
+		if (StringUtils.isEmptyOrNull(guid)) {
+			throw new DrumbeatWebException(Status.BAD_REQUEST, "Param 'guid' is required", null);
+		}
+		
+		if (action.equalsIgnoreCase(IFC_GUID_ACTION_COMPRESS)) {
+			return GuidCompressor.compressGuidString(guid);			
+		} else if (action.equalsIgnoreCase(IFC_GUID_ACTION_DECOMPRESS)) {			
+			return GuidCompressor.uncompressGuidString(guid);
+		} else {
+			throw new DrumbeatWebException(Status.BAD_REQUEST, "Invalid param 'action'", null);			
+		}
+		
+		
+	}
+	
+	@GET
+	@Path("/ifcguid")	
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String compressIfcGuid2(
+			@QueryParam("action") String action,
+			@QueryParam("guid") String guid)
+	{
+		return compressIfcGuid(action, guid);
+	}
+
+}
